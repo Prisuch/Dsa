357ea4d492961d2145e996bcd5e5abe3b00f4d62
Prisuch <prithusach36@gmail.com>
2026-06-09 15:37:59 +0530

    Armstrong Number

--- Understanding_C++/Check_ArmStrong_num.cpp
+++ Understanding_C++/Check_ArmStrong_num.cpp
@@ -1,1 +1,35 @@
+#include <string>
+#include <iosystem>
+#include <stdexcept>
 
+bool Armstrongnum(int num)
+{
+	std::string indexnum = std::to_string(num);
+	int len = indexnum.length();
+	int totalsum = 0;
+	for (int i = 0; i < len; i++)
+	{
+		int tempnum = 0;
+		int singledig = indexnum[i] - '0';
+		tempnum = std::pow(singledig, len);
+		totalsum += tempnum;
+	}
+	if (totalsum == num)return true;
+	return false;
+}
+
+int main()
+{
+	int num;
+	bool wannachange = false;
+	std::string question;
+	while (!wannachange)
+	{
+		std::cout << "Enter a number - ";
+		std::cin >> num;
+		std::cout << "Is the number armstrong (0 = False, 1 = True)" << Armstrongnum(num);
+		std::cout << "Want to get out of code - ";
+		std::cin >> question;
+		if (question == "yes") wannachange = true;
+	}
+}
